5d1b2893f95565e8a951dd94f663a7b9203f9379
Harmanjeet Singh <harmangahir@gmail.com>
2022-12-27 22:29:03 +0530

    This commit involves readme and requirements.txt

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def predict():
     data=request.form.values()
     processed_text = processed_data.preprocessing(data)
     output = label_fit.inverse_transform(model.predict(tfidf_vector.transform([processed_text])))[0]
-    return render_template("result.html",prediction_text="The predicted category of article is {}".format(output))
+    return render_template("index.html",prediction_text="Predicted category of article  is {}".format(output))
+    
     
 if __name__=="__main__":
     app.run(debug=True)
