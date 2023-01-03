7d1a377148d507c84dadc69cd5ce23de0c186d47
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-03 15:40:10 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -35,6 +35,7 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=request.form['article']
+    #model_name = request.form['ml_model']
     processed_text = processed_data.preprocessing(data)
     output = label_fit.inverse_transform(model.predict(tfidf_vector.transform([processed_text])))[0]
     return render_template("index.html",prediction_text="Predicted category with model for article  is:{}".format(output))
