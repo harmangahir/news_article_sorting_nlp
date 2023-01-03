62bc2d131ebb6702edd1e2266b5d65196efb714d
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-03 14:51:14 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -37,7 +37,6 @@ def predict():
     data=request.form['article']
     processed_text = processed_data.preprocessing(data)
     output = label_fit.inverse_transform(model.predict(tfidf_vector.transform([processed_text])))[0]
-    print('output')
     return render_template("index.html",prediction_text="Predicted category with model for article  is:{}".format(output))
     
     
