339d00ca3b40a799bd927eec2cc2c194620444d5
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-03 22:17:23 +0530

    The web app is ready

--- static/js/index.js
+++ static/js/index.js
@@ -33,3 +33,15 @@ function dataSent() {
   } 
 return false;
 }
+
+const btn = document.getElementById('submitBtn');
+
+btn.addEventListener('click', () => {
+  const form = document.getElementById('form');
+
+  if (form.style.visibility === 'hidden') {
+    form.style.visibility = 'visible';
+  } else {
+    form.style.visibility = 'hidden';
+  }
+});
