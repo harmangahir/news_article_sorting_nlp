f9e6bc686822de0eb20c4449d00a917959bb06be
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-03 15:13:01 +0530

    The web app is ready

--- static/js/index.js
+++ static/js/index.js
@@ -23,15 +23,6 @@ selectTwo.addEventListener("change", () => {
     } 
   })
 
-  element.addEventListener("change", (e) => {
-    const value = e.target.value;
-    const text = element.options[element.selectedIndex].text;
-   
-    if (value) {
-      document.getElementById("pick").textContent = `Value Selected: ${value}`;
-    } else {
-      document.getElementById("pick").textContent = "";
-    }
-  });
+
 
   
