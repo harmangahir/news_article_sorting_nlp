fd29385779ff10223d9df12caa96b0b8789347d6
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-03 16:17:18 +0530

    The web app is ready

--- static/js/index.js
+++ static/js/index.js
@@ -2,29 +2,32 @@ let inputText = document.querySelector("#text");
 let selectOne = document.querySelector("#ml-models");
 let selectTwo = document.querySelector("#dl-models");
 let buttonSelect = document.querySelector("#submitBtn");
-selectOne.addEventListener("change", () => {  
+
+function selectOne() {
   if (selectOne.value!="") {
     selectTwo.disabled = true;
-    alert('select One')
-    return false; }  
+    }  
 else {
     selectTwo.disabled = false;
+  }
+  return false;
+}
+
+function selectTwo() {
+  if (selectTwo.value!="") {
+    selectOne.disabled = true;
+    alert('select Two')
+           
+} else {
+    selectOne.disabled = false;
+    
+}
     return false;
 }
-})
-selectTwo.addEventListener("change", () => {  
-    if (selectTwo.value!="") {
-        selectOne.disabled = true;
-        alert('select Two')
-        return false;       
-    } else {
-        selectOne.disabled = false;
-        return false;
-    }
-  })
-  buttonSelect.addEventListener("click",()=>{    
-    if(inputText.value!="") {
-        document.querySelector(".table").id="showTable";
-        return false;        
-    } 
-  })
+
+function dataSent() {
+  if(inputText.value!="") {
+    document.querySelector(".table").id="showTable";
+  } 
+return false;
+}
