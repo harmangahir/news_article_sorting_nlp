a27079e318a56d64d5d6d8a8c3a7c84c0c88e0b6
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-05 00:10:45 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -167,7 +167,48 @@ def predict():
         processed_text = processed_data.preprocessing(data)
         output = label_fit.inverse_transform(model.predict(tfidf_vector.transform([processed_text])))[0]
         return render_template("index.html", models_name=model_name, prediction_text=output)
+
+
+
+@app.route('/predict_all',methods=['POST'])
+def predict_all():
+    data= request.form['article']
+    BNB_model = pk.load(open('model_BNB.pk','rb'))
+    MNB_model = pk.load(open('model_MNB.pk','rb'))
+    processed_text = processed_data.preprocessing(data)
+    p_tfidf = tfidf_vector.transform(processed_text)
     
+    
+    BNB_result = BNB_model.predict(p_tfidf)[0]
+    BNB_output = label_fit.inverse_transform(BNB_model.predict(tfidf_vector.transform(processed_text)))[0]
+    BNB_score = BNB_model.predict_proba(tfidf_vector.transform(processed_text))[BNB_result]
+    BNB_business_percentage=round(BNB_score[0]*100,2)
+    BNB_tech_percentage=round(BNB_score[1]*100,2)
+    BNB_politics_percentage=round(BNB_score[2]*100,2)
+    BNB_sport_percentage=round(BNB_score[3]*100,2)
+    BNB_entertainment_percentage=round(BNB_score[4]*100,2)
+
+    MNB_result = BNB_model.predict(p_tfidf)[0]
+    MNB_output = label_fit.inverse_transform(MNB_model.predict(tfidf_vector.transform(processed_text)))[0]
+    MNB_score = MNB_model.predict_proba(tfidf_vector.transform(processed_text))[MNB_result]
+    MNB_business_percentage=round(MNB_score[0]*100,2)
+    MNB_tech_percentage=round(MNB_score[1]*100,2)
+    MNB_politics_percentage=round(MNB_score[2]*100,2)
+    MNB_sport_percentage=round(MNB_score[3]*100,2)
+    MNB_entertainment_percentage=round(MNB_score[4]*100,2)
+    
+    
+    
+    
+    
+    
+    
+    return render_template("index.html", BNB_model_name=BNB_model,BNB_business=BNB_business_percentage, BNB_tech=BNB_tech_percentage,BNB_politics=BNB_politics_percentage,BNB_sport=BNB_sport_percentage,BNB_entertainment=BNB_entertainment_percentage ,BNB_predicted_category=BNB_output,MNB_model_name=MNB_model,MNB_business=MNB_business_percentage, MNB_tech=MNB_tech_percentage,MNB_politics=MNB_politics_percentage,MNB_sport=MNB_sport_percentage,MNB_entertainment=MNB_entertainment_percentage ,MNB_predicted_category=MNB_output)
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
     #app.run(host = '0.0.0.0', port=8080)
