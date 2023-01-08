ea6c1f503a668c2adc8ac79ac826ce105f365ade
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-08 22:55:22 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -15,32 +15,24 @@ def home():
     return render_template('index.html')
 
 @app.route('/predict_api',methods=['POST'])
-
-# def predict_category(model_name,processed_text):
-#     p_tfidf = tfidf_vector.transform(processed_text)
-#     result1 = model_name.predict(p_tfidf)[1]
-#     score = model_name.predict_proba(tfidf_vector.transform(processed_text))[result1]
-#     business_percentage=round(score[0]*100,2)
-#     tech_percentage=round(score[1]*100,2)
-#     politics_percentage=round(score[2]*100,2)
-#     sport_percentage=round(score[3]*100,2)
-#     entertainment_percentage=round(score[4]*100,2)
-#     model_prediction = label_fit.inverse_transform(model_name.predict(tfidf_vector.transform(processed_text)))[0]
     
-#     return business_percentage, tech_percentage,politics_percentage,sport_percentage,entertainment_percentage,model_prediction
+    
 
-def predict_api():
-    data= request.json
-    print('a')
+# def predict_api():
+    
+    
+
+#     print('a')
+#     return jsonify(tuple1[0])
     # processed_text = processed_data.preprocessing(data)
-    # model = pk.load(open('models/model_BNB.pk','rb'))
+    # 
     # print(model)
     # print(processed_text)
-    # tuple1 = predict_category(model,processed_text)
+    # 
     # return jsonify(tuple1[5])
 #     data=request.json
 #     print(data)
-#     processed_text = processed_data.preprocessing(data)
+#     
 #     print(processed_text)
 #     output = label_fit.inverse_transform(model.predict(tfidf_vector.transform([processed_text])))[0]
     
@@ -60,18 +52,25 @@ def predict_api():
         
     
 
-#@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST'])
 
 
-# def predict():
-#     data= request.form['article']
-#     processed_text = processed_data.preprocessing(data)
-#     model_name = request.form['ml_model']
-    
-
-#     if(model_name == 'BNB'):
-#         model = pk.load(open('models/model_BNB.pk','rb'))
-#         tuple1 = predict_category(model,processed_text)
+def predict():
+    data= request.form['article']
+    model_name = request.form['ml_model']
+    processed_text = processed_data.preprocessing(data)
+    if(model_name == 'BNB'):
+        model = pk.load(open('models/model_BNB.pk','rb'))
+        p_tfidf = tfidf_vector.transform(processed_text)
+        result1 = model.predict(p_tfidf)[1]
+        score = model.predict_proba(tfidf_vector.transform(processed_text))[result1]
+        business_percentage=round(score[0]*100,2)
+        tech_percentage=round(score[1]*100,2)
+        politics_percentage=round(score[2]*100,2)
+        sport_percentage=round(score[3]*100,2)
+        entertainment_percentage=round(score[4]*100,2)
+        model_prediction = label_fit.inverse_transform(model.predict(tfidf_vector.transform(processed_text)))[0]
+        return render_template("index.html", model_name = model,BNB_business_prob = business_percentage, BNB_tech_prob=tech_percentage,BNB_political_prob = politics_percentage,BNB_sport_prob = sport_percentage,BNB_entertainment_prob = entertainment_percentage,predicted_category = model_prediction)
         
 #         # return render_template("index.html", BNB_model_name=BNB_model,BNB_business=BNB_business_percentage, BNB_tech=BNB_tech_percentage,BNB_politics=BNB_politics_percentage,BNB_sport=BNB_sport_percentage,BNB_entertainment=BNB_entertainment_percentage ,BNB_predicted_category=BNB_output,MNB_model_name=MNB_model,MNB_business=MNB_business_percentage, MNB_tech=MNB_tech_percentage,MNB_politics=MNB_politics_percentage,MNB_sport=MNB_sport_percentage,MNB_entertainment=MNB_entertainment_percentage ,MNB_predicted_category=MNB_output)
         
