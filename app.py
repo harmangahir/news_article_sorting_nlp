2682e18f32554a208dede00fb401af45332e3019
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-24 10:39:23 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -232,6 +232,6 @@ def predict_all():
 
 
 if __name__=="__main__":
-    #app.run(debug=True)
-    app.run(host = '0.0.0.0', port=8080)
+    app.run(debug=True)
+    #app.run(host = '0.0.0.0', port=8080)
    
