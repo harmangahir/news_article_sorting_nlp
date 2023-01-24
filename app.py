c6a9ede168690532b5a7440081c55ab31a558ece
Harmanjeet Singh <harmangahir@gmail.com>
2023-01-24 10:33:21 +0530

    The web app is ready

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ app=Flask(__name__)
 ## Load the model
 
 tfidf_vector = pk.load(open('TF-IDF\Tfidf_Vectorizer.pk','rb'))
-label_fit = pk.load(open('/Labels/label_fit.pk','rb'))
-tokenizer = pk.load(open("/Tokenizer/tokenizer.pk", "rb"))
+label_fit = pk.load(open('Labels\label_fit.pk','rb'))
+tokenizer = pk.load(open("Tokenizer\tokenizer.pk", "rb"))
 label_encoder_pk = pk.load(open("Labels\label_encoder.pk", "rb"))
 
 
